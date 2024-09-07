765e162c8656044ff8f7f21b6b0412680b0cbd74
kunal sharma <kunal.ucet@gmail.com>
2024-09-07 10:07:05 +0530

    Set up Express server with routes and database connection

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const emiRoutes = require('./routes/emiRoutes');
+const sequelize = require('./config/db');
+const errorHandler = require('./middlewares/errorHandler');
+
+const app = express();
+app.use(express.json());
+
+// EMI Routes
+app.use('/api', emiRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Sync database and start server
+sequelize.sync().then(() => {
+  app.listen(3000, () => console.log('Server running on port 3000'));
+}).catch(err => console.error('Unable to connect to the database:', err));
