3b60e200fc5a92dd07f92298b259736775b4c97b
kunal sharma <kunal.ucet@gmail.com>
2024-09-07 11:09:14 +0530

    added db port and timeout

--- config/db.js
+++ config/db.js
@@ -3,8 +3,13 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 5432, // Add port number with a default value for PostgreSQL
   dialect: 'postgres',
+  dialectOptions: {
+    connectTimeout: 60000 // Increase the connection timeout to 60 seconds
+  },
   logging: false, // Disable Sequelize logs in production
 });
+
 
 module.exports = sequelize;
