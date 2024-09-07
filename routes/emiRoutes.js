4507c15a41776872da753f29d8b5ad4d0ec22fa6
kunal sharma <kunal.ucet@gmail.com>
2024-09-07 11:01:18 +0530

    deleted routes/emiRoutes.js

--- routes/emiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const emiController = require('../controllers/emiController');
-const router = express.Router();
-
-router.post('/calculate-emi', emiController.calculateEMI);
-router.get('/emis', emiController.getAllLoans);
-router.get('/emi/:id', emiController.getLoanById);
-
-module.exports = router;
